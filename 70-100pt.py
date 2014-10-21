ab3c374553c298757275938925d8f4333d970af6
Luke Cooper <luke.cooper96@gmail.com>
2014-10-21 14:23:18 -0500

    lab 10 final version

--- 70-100pt.py
+++ 70-100pt.py
@@ -25,19 +25,20 @@ drawpad.grid(row=0, column=1)
 # Insert your code here to draw the house!
 
 #70 point outline of house and roof
-rectangle1 = drawpad.create_rectangle(200,200,400,400)
+rectangle1 = drawpad.create_rectangle(200,200,400,400,fill="red")
 line1 = drawpad.create_line(200,200,300,100)
 line2 = drawpad.create_line(300,100,400,200)
 #80 point windows and door
-window1 = drawpad.create_rectangle(225,300,275,250)
-window2 = drawpad.create_rectangle(325,300,375,250)
-door = drawpad.create_rectangle(275,400,325,325)
+window1 = drawpad.create_rectangle(225,300,275,250,fill="white")
+window2 = drawpad.create_rectangle(325,300,375,250,fill="white")
+door = drawpad.create_rectangle(275,400,325,325,fill="white")
 #90 point doorknob and chimney
-doorknob = drawpad.create_oval(285,360,295,370)
+doorknob = drawpad.create_oval(285,360,295,370,fill="white")
 line3 = drawpad.create_line(225,175,225,50)
 line4 = drawpad.create_line(250,150,250,50)
 line5 = drawpad.create_line(225,50,250,50)
-
+#100 point grass and house color
+grass = drawpad.create_rectangle(200,400,400,500, fill="green")
 
 
 
